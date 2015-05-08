46efe87f52ca17925decb386e8d8870d52a185bd
baghnac <baghnac@gmail.com>
2015-05-08 22:44:03 +0800

    优化创造： 1. 去掉TOTAL_RESOURCE_TYPE，由表配置自动生成Pool 2. 由表配置创建实际

--- src/resource_manager.cpp
+++ src/resource_manager.cpp
@@ -13,52 +13,55 @@ private:
     int index_;
 };
 
-#define TOTAL_RESOURCE_TYPE 3
 #define MAX_RESOURCE_OBJS_OF_ONE_TYPE 20
 
 #define TYPE_A_ID 0
 #define TYPE_B_ID 1
 #define TYPE_C_ID 2
 
+#define RESOURCE_TYPE_TABLE(_, __, ___) \
+    _(TYPE_A_ID, ResourceTypeA, __, ___) \
+    _(TYPE_B_ID, ResourceTypeB, __, ___) \
+    _(TYPE_C_ID, ResourceTypeC, __, ___) 
+
 struct ResourceTypeA : public Resource{
 	static int getTypeId(){return TYPE_A_ID;};
     ResourceTypeA(int index):Resource(index){};
-    void realTypeBusiness(){cout << "this is business of type A " << endl;};
+    void realTypeBusiness(){cout << "this is business of type A. index is " << getIndex()  << endl;};
 };
 
 struct ResourceTypeB : public Resource{
 	static int getTypeId(){return TYPE_B_ID;};
     ResourceTypeB(int index):Resource(index){};
-    void realTypeBusiness(){cout << "this is business of type B " << endl;};
+    void realTypeBusiness(){cout << "this is business of type B. index is " << getIndex() << endl;};
 };
 
 struct ResourceTypeC : public Resource{
 	static int getTypeId(){return TYPE_C_ID;};
     ResourceTypeC(int index):Resource(index){};
-    void realTypeBusiness(){cout << "this is business of type C " << endl;};
+    void realTypeBusiness(){cout << "this is business of type C. index is " << getIndex()  << endl;};
 };
 
 struct ResourcePool{
+    int type_id;
     Resource* ro[MAX_RESOURCE_OBJS_OF_ONE_TYPE];
 };
 
-ResourcePool resourcePool[TOTAL_RESOURCE_TYPE] = {0};
+#define CREATE_RESOURCE(type_id, Type, res_index, res) \
+    case type_id: {   \
+        res = (Resource*) (new Type(res_index)); \
+        break; \
+    }
+
+#define CREATE_RESOURCE_POOL(type_id, Type, _, __) {type_id, NULL},
+
+ResourcePool resourcePool[] = {RESOURCE_TYPE_TABLE(CREATE_RESOURCE_POOL, NULL, NULL)};
+
 
 void CreateOneResourceType(int type_id, int res_index){
     Resource* res = NULL;
     switch(type_id) {
-        case 0: {
-            res = (Resource*) (new ResourceTypeA(res_index));
-            break;
-        }
-        case 1: {
-            res = (Resource*) (new ResourceTypeB(res_index));
-            break;
-        }
-        case 2: {
-            res = (Resource*) (new ResourceTypeC(res_index));
-            break;
-        }
+        RESOURCE_TYPE_TABLE(CREATE_RESOURCE, res_index, res);
 		default: {
             break;
         }
@@ -72,14 +75,14 @@ void CreateRepResources(int rep_id[], int rep_num){
      for(long loop = 0; loop < rep_num; loop++){
         switch (rep_id[loop]) {
             case REPOSITORY_TYPE_ONE: {
-                CreateOneResourceType(ResourceTypeA::getTypeId(), 1);
-                CreateOneResourceType(ResourceTypeB::getTypeId(), 1);
+                CreateOneResourceType(TYPE_A_ID, 1);
+                CreateOneResourceType(TYPE_B_ID, 1);
                 break;
             }
             case REPOSITORY_TYPE_TWO: {
-                CreateOneResourceType(ResourceTypeA::getTypeId(), 2);
-                CreateOneResourceType(ResourceTypeB::getTypeId(), 2);
-                CreateOneResourceType(ResourceTypeC::getTypeId(), 2);
+                CreateOneResourceType(TYPE_A_ID, 2);
+                CreateOneResourceType(TYPE_B_ID, 2);
+                CreateOneResourceType(TYPE_C_ID, 2);
                 break;
             }
         }
